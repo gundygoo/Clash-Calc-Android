708054ce6a9a3033bc248b1e2eed45ed4bbd4ce3
Erik Gunderson <gundy.goo@gmail.com>
2015-12-08 16:17:19 -0600

    Initial Work
    :)

---
+++ ClashCalc/app/src/main/java/clashtroopcalc/clashcalc/TroopInput.java
@@ -0,0 +1,13 @@
+package clashtroopcalc.clashcalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TroopInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_troop_input);
+    }
+}
